7e5422f09e05c28876054f6b4701d86ede33b21d
Rodrigo Anastacio <rodrigo.anastacio@dtidigital.com.br>
2025-02-02 00:39:58 -0300

    Fix: TV Channels Grid

--- src/pages/TvChannels.tsx
+++ src/pages/TvChannels.tsx
@@ -35,7 +35,7 @@ const TvChannels = () => {
           </p>
         </div>
 
-        <div className="space-y-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {channels.map((item, index) => (
             <div key={index} className="">
               <ChannelItem {...item} />
